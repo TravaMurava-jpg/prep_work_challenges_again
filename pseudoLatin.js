268e2de26d1a156cfc2db6731133ac064992971a
TravaMurava-jpg <55783365+TravaMurava-jpg@users.noreply.github.com>
2022-11-13 17:43:43 +0600

    Add files via upload

--- pseudoLatin.js
+++ pseudoLatin.js
@@ -8,3 +8,17 @@ makeLatin("I speak latin")
 // should return:
 "Iay peaksay atinlay"
 
+// massiv arrayev kak otdelnix slov
+// 
+
+function makeLatin(word){
+    var arr = word.split(" ");
+    for (var i = 0; i < arr.length; i++){
+        var firstLetter = arr[i][0];
+        arr[i] = arr[i].replace(firstLetter, '');
+        arr[i] += firstLetter + "ay";
+    }
+    return arr.join(" ");
+}
+
+console.log(makeLatin("I speak latin"));
